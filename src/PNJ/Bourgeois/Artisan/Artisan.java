80370da9c9d18a3e82b8e5d5c6a66743517e6df6
arnaud <arnaud.trinite@gmail.com>
2019-10-09 10:35:51 +0200

    Ajouts de classe Bourgeois :
    Artisan
    Merchant

---
+++ src/PNJ/Bourgeois/Artisan/Artisan.java
@@ -0,0 +1,9 @@
+package PNJ.Bourgeois.Artisan;
+
+import PNJ.Bourgeois.Bourgeois;
+
+public class Artisan extends Bourgeois {
+    public Artisan(String job) {
+        this.setJob(job);
+    }
+}
---
+++ src/PNJ/Bourgeois/Merchant/Merchant.java
@@ -0,0 +1,9 @@
+package PNJ.Bourgeois.Merchant;
+
+import PNJ.Bourgeois.Bourgeois;
+
+public class Merchant extends Bourgeois {
+    public Merchant(String job) {
+        this.setJob(job);
+    }
+}
